58f1f70dc3537f4941c2fe1a8d2fee8c3a55a1a8
Youngjae99 <elite1111cyj@gmail.com>
2020-07-21 03:32:27 +0900

    logo

--- app/src/main/java/com/example/project2/Fragment1.java
+++ app/src/main/java/com/example/project2/Fragment1.java
@@ -10,17 +10,23 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.AdapterView;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.ImageButton;
+import android.widget.ImageView;
 import android.widget.ListView;
+import android.widget.TextView;
+import android.widget.Toast;
 
+import androidx.appcompat.app.AlertDialog;
 import androidx.fragment.app.Fragment;
 import androidx.recyclerview.widget.RecyclerView;
 
+import com.bumptech.glide.Glide;
 import com.example.project2.Retrofit.IMyService;
 import com.example.project2.Retrofit.RetrofitClient;
 
 import org.json.JSONArray;
-import org.json.JSONException;
+import org.w3c.dom.Text;
 
 import java.util.ArrayList;
 
@@ -28,9 +34,6 @@ import io.reactivex.android.schedulers.AndroidSchedulers;
 import io.reactivex.disposables.CompositeDisposable;
 import io.reactivex.functions.Consumer;
 import io.reactivex.schedulers.Schedulers;
-import retrofit2.Call;
-import retrofit2.Callback;
-import retrofit2.Response;
 import retrofit2.Retrofit;
 
 public class Fragment1 extends Fragment{
@@ -144,12 +147,54 @@ public class Fragment1 extends Fragment{
                 startActivity(newPostIntent);
             }
         });
-
         return v;
     }
 
-    public void doSelectFriend(Person p)
-    {
-        Log.e("####", p.getName() + ", " + p.getNumber());
+    public void doSelectFriend(Person p) {
+        Log.d("Clicked", p.getName() + ", " + p.getNumber() + ", " + p.getEmail());
+        //Intent intent = new Intent(getContext(), PopupActivity.class);
+        //intent.putExtra("user", p.getName());
+        //intent.putExtra("data", "Test Popup");
+        //startActivityForResult(intent, 1);
+
+        AlertDialog.Builder mBuilder = new AlertDialog.Builder(getContext());
+        View mView = getLayoutInflater().inflate(R.layout.popup_user, null);
+        Button send = (Button) mView.findViewById(R.id.send);
+        TextView txtText = (TextView)mView.findViewById(R.id.txtText);
+        TextView user = (TextView)mView.findViewById(R.id.userTo);
+        //txtText.setText("익명으로 전달됩니다");
+        user.setText("To. "+p.getName());
+
+        for(int i=1; i<=3; i++){
+
+        }
+        ImageView img1 = mView.findViewById(R.id.profile1);
+        ImageView img2 = mView.findViewById(R.id.profile2);
+        ImageView img3 = mView.findViewById(R.id.profile3);
+        Glide.with(getContext())
+                //.load(imageUrls.get(i).getImageUrl()) // 웹 이미지 로드
+                .load("qweqweqwe") // 이미지 로드
+                .error(R.drawable.noimage)
+                .override(500,500) //해상도 최적화
+                .thumbnail(0.3f) //섬네일 최적화. 지정한 %만큼 미리 이미지를 가져와 보여주기
+                .centerCrop() // 중앙 크롭
+                .into(img1);
+
+        mBuilder.setView(mView);
+        final AlertDialog dialog = mBuilder.create();
+        dialog.show();
+        send.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Log.d("clicked","button");
+                // Send 버튼 눌렀을 때 서버로 저장하는 코드 작성 필요
+
+
+
+                // ====================
+                Toast.makeText(getContext(), "익명으로 "+ p.getName()+"님께 메세지가 전송되었습니다", Toast.LENGTH_SHORT).show();
+                dialog.dismiss(); // 팝업 종료
+            }
+        });
     }
 }
